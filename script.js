cea8650ab93bfb2ea537d78d80638f2afbf9ce64
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2023-10-16 10:48:04 +0800

    hehe

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const quoteContainer = document.createElement('div');
+quoteContainer.classList.add('flex');
+document.appendChild(quoteContainer);
+
 fetch('https://programming-quotesapi.vercel.app/api/random')
   .then(response => response.json())
   .then(quote => console.log(quote));
