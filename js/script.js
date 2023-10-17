fb985b6ab87ad5905090a7852601d0793d3dcfb5
ttiramisu <106864282+ttiramisu@users.noreply.github.com>
2023-10-17 13:23:40 +0800

    random date

--- js/script.js
+++ js/script.js
@@ -90,10 +90,59 @@ fetch('https://programming-quotesapi.vercel.app/api/random')
   })
   .catch(error => console.error(error));
 
-const today = function() {
-  let now = new Date().toLocaleDateString('en-us', { month:"numeric", day:"numeric"});
+const today = function () {
+  let now = new Date().toLocaleDateString('en-us', { month: "numeric", day: "numeric" });
   month = now.substring(0, 2);
   day = now.substring(3, 5);
+
+  fetch(`https://byabbe.se/on-this-day/${month}/${day}/events.json`)
+    .then(response => response.json())
+    .then(data => {
+      eventContainer.innerHTML = '';
+      const { wikipedia, date } = data;
+
+      dateContainer.appendChild(displayDateHeader);
+      dateContainer.appendChild(displayDate);
+      displayDate.textContent = date;
+
+      wikiContainer.appendChild(displayWikiHeader);
+      wikiContainer.appendChild(displayWiki);
+      displayWiki.textContent = wikipedia;
+      displayWiki.target = '_blank';
+      displayWiki.href = wikipedia;
+
+      const last5Events = data.events.slice(-5);
+      console.log(last5Events);
+
+      last5Events.forEach((event, index) => {
+        const eventElement = document.createElement("div");
+        eventElement.innerHTML =
+          `<div>
+            <h5>${index + 1}.</h5>
+            <p><strong>Description:</strong> ${event.description}</p>
+            <p><strong>Year:</strong> ${event.year}</p>
+            <p class="mb-4"><strong>Wikipedia:</strong> ${event.wikipedia.length > 0 ? event.wikipedia.map(w =>
+            `<a href="${w.wikipedia}" target="_blank">${w.title}</a>`).join(', ')
+            : "None"}</p>
+          </div>`;
+        eventContainer.appendChild(eventElement);
+      });
+    })
+    .catch(error => console.error(error));
+};
+
+const randomDate = function () {
+  month = Math.floor(Math.random() * 13);
+  day = Math.floor(Math.random() * 31);
+
+  if (month === 0 || day === 0) {
+    month = Math.floor(Math.random() * 13);
+    day = Math.floor(Math.random() * 31);
+  }
+
+  if (month === 2 && day > 28) {
+    day = Math.floor(Math.random() * 29);
+  }
 
   fetch(`https://byabbe.se/on-this-day/${month}/${day}/events.json`)
     .then(response => response.json())
